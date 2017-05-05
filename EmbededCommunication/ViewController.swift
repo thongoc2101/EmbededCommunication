0ac5aab769f98b052bacbdde2b319427079359d1
Phan Thi Ngoc Cam <camphan@Phans-Mac.local>
2017-05-05 13:15:46 +0700

    Initial Commit

---
+++ EmbededCommunication/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  EmbededCommunication
+//
+//  Created by Phan Thi Ngoc Cam on 5/5/17.
+//  Copyright © 2017 ngoccam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var messageField: UITextField!
+    
+    
+    @IBAction func button(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
