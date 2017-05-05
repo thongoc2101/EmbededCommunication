178964ec0916f6bdae21f216cac61f34a1bee989
Phan Thi Ngoc Cam <camphan@Phans-Mac.local>
2017-05-05 13:16:47 +0700

    Commit

--- EmbededCommunication/ViewController.swift
+++ EmbededCommunication/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     @IBAction func button(_ sender: Any) {
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
